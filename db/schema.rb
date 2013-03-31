a66a38764665e93cb550a41b8985cf240336c6e9
Lesosa Kiwa <leskiwa1@umbc.edu>
2013-03-31 04:16:01 -0400

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,9 @@ ActiveRecord::Schema.define(:version => 20130316141211) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "password_digest"
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end

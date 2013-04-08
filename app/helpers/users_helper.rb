4c9dd9118b83db0a895f749af7e22a8f903c90be
Lesosa Kiwa <leskiwa1@umbc.edu>
2013-04-07 23:15:43 -0400

    Finish sign in

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,22 +1,10 @@
-  module UsersHelper
+module UsersHelper
 
-<<<<<<< HEAD
-    # Returns the Gravatar (http://gravatar.com/) for the given user.
-    def gravatar_for(user, options = { size: 40})
-        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-	size = options[:size]
-	# size = 40
-        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-        image_tag(gravatar_url, alt: user.name, class: "gravatar")
-    end
-=======
-  # Returns the Gravatar
+  # Returns the Gravatar for the given user.
   def gravatar_for(user, options = { size: 50 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
-    # size = 40
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
->>>>>>> 288c182e112c181a65b90a69bd864cf0eac1a2c6
   end
 end
